fb61e40171b5e526a4d0edf4cc7a65dd447529a2
RodneyCStead <RodCStead@gmail.com>
2024-07-17 00:43:05 -0230

    connected to database, and ejs view of all data displayed to server on port 3000

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ const methodOverride = require('method-override');
 const app = express();
 const port = 3000;
 
+global.DEBUG = true;  
+const { getTasks } = require('./services/m.tasks.dal');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 
-// Define routes here
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await getTasks();
+    // Render the tasksView.ejs file and pass the tasks data to it
+    res.render('tasks', { tasks });
+  } catch (error) {
+    console.error('Error fetching tasks:', error);
+    res.status(500).send('Server error');
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);

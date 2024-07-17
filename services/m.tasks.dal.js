88dd64637b5a70ad717d7086dca5c18c51acb0a1
RodneyCStead <RodCStead@gmail.com>
2024-07-17 01:30:28 -0230

    POST method successful

--- services/m.tasks.dal.js
+++ services/m.tasks.dal.js
@@ -5,6 +5,14 @@ var getTasks = async () => {
     return rows;
   };
 
+var addTask = async (task) => {
+  const { name, description, status, due_date } = task;
+  await pool.query('INSERT INTO Tasks (name, description, status, due_date) VALUES ($1, $2, $3, $4)', [name, description, status, due_date]);
+  };
 
-module.exports = {getTasks};
+
+module.exports = {
+  getTasks,
+  addTask
+};
 

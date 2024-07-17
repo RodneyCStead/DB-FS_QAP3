ff20242eaebd48d8d9757d733939bdb5a8ead456
RodneyCStead <RodCStead@gmail.com>
2024-07-17 12:11:01 -0230

    PUT is operational as the replace link, added the other needed views as fblank code for DELETE and PATCH

--- services/m.tasks.dal.js
+++ services/m.tasks.dal.js
@@ -1,7 +1,7 @@
 const pool = require("./tasks_db");
 
 var getTasks = async () => {
-    const { rows } = await pool.query('SELECT * FROM Tasks');
+    const { rows } = await pool.query('SELECT * FROM Tasks ORDER BY id ASC');
     return rows;
   };
 
@@ -15,10 +15,15 @@ var updateTask = async (task) => {
   await pool.query('UPDATE Tasks SET name=$2, description=$3, status=$4, due_date=$5 WHERE id=$1', [id, name, description, status, due_date]);
   };
 
+var getTaskById = async (id) => {
+  const { rows } = await pool.query('SELECT * FROM Tasks WHERE id=$1', [id]);
+  return rows[0];
+  };
 
 module.exports = {
   getTasks,
   addTask,
-  updateTask
+  updateTask,
+  getTaskById
 };
 
